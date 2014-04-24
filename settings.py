476fd4a0093ad12e3b29075ef28fee74003cf49c
Nikhil Anand <anand.nikhil@gmail.com>
2014-04-24 03:46:24 -0500

    No. of workers can be adjusted in settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@ HOST="0.0.0.0"
 PORT=5000
 DEBUG = True
 MONGODB_SETTINGS = {'DB': "goosepaper", 'PORT': 27017}
+NUMBER_OF_WORKERS = 10
 
 # Use if you want to host at localhost:5000/things_i_read
 # If you're on a dedicated subdomain, comment this out entirely.
@@ -16,3 +17,4 @@ SECRET_KEY = "r/hV9t14EwGfUyTmSGy1jYFfQx526ToQ"
 
 SUMMARY_LENGTH = 250
 ARTICLES_PER_PAGE = 50
+
